55eca15ca204d4d86b665ebfacd3b86b05596832
Denys <werdumbox@gmail.com>
2026-06-21 23:00:26 +0200

    created classes and overrode methods doWork and stopWorks. Created Machine's array and called those methods in the loop.

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Bulldozer extends Machine {
+
+    public void doWork() {
+        System.out.println("Buildozer started its work");
+    }
+
+    public void stopWork() {
+        System.out.println("Buildozer stop its work");
+    }
+}
